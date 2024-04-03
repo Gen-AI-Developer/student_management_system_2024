4d02aaecfdfff131f8986893ceaaf4e2b41cdd88
Syed Safdar Ali Shah <116350058+Gen-AI-Developer@users.noreply.github.com>
2024-04-03 23:39:13 +0500

    updatetoday

--- source/index.ts
+++ source/index.ts
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
-interface student_structure {
-    name: String,
-    gender: 'Male' | 'Female',
-    fatherName: String,
-    id: Number,
-    Semester: String,
-    Department: String,
+interface StudentStructure {
+    name: string;
+    gender: "Male" | "Female";
+    fatherName: string;
+    id: number;
+    Semester: string;
+    Department: string;
 }
 
-class SMS implements student_structure {
-     constructor new SMS(name,gender,fathername,id,semester,department){
+class SMS implements StudentStructure {
+    name: string;
+    gender: "Male" | "Female";
+    fatherName: string;
+    id: number;
+    Semester: string;
+    Department: string;
 
+    constructor(name: string, gender: "Male" | "Female", fatherName: string, id: number, Semester: string, Department: string) {
+        this.name = name;
+        this.gender = gender;
+        this.fatherName = fatherName;
+        this.id = id;
+        this.Semester = Semester;
+        this.Department = Department;
     }
+
 }
